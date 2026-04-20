3f47013c6485f8dc30d8b79a3e2513bd061d3253
Keiner Mendoza <keinermendoza@gmail.com>
2026-04-20 11:03:13 -0400

    fix: add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,30 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
-export default defineConfig({
-    site: "https://abogadamagdiele.com",
+import cloudflare from "@astrojs/cloudflare";
 
-    fonts: [
-       {
-            provider: fontProviders.google(),
-            name: "Comfortaa",
-            cssVariable: "--ff-body",
-            weights: [400, 500, 800],
-            styles: ["normal"]
-    
-        },
-        {
-            provider: fontProviders.google(),
-            name: "Plus Jakarta Sans",
-            cssVariable: "--ff-title",
-            weights: [800],
-            styles: ["normal"]
-    
-        },
-    ]
+export default defineConfig({
+  site: "https://abogadamagdiele.com",
+
+  fonts: [
+     {
+          provider: fontProviders.google(),
+          name: "Comfortaa",
+          cssVariable: "--ff-body",
+          weights: [400, 500, 800],
+          styles: ["normal"]
+  
+      },
+      {
+          provider: fontProviders.google(),
+          name: "Plus Jakarta Sans",
+          cssVariable: "--ff-title",
+          weights: [800],
+          styles: ["normal"]
+  
+      },
+  ],
+
+  adapter: cloudflare(),
+  output: "static",
 });
