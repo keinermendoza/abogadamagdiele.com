96c66f172bd565dbd383bc4c4314bf6b9377ba07
Keiner Mendoza <keinermendoza@gmail.com>
2026-04-20 10:49:20 -0400

    fix: remove cloudflare adapter, also add site property to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
-    adapter: cloudflare(),
+    site: "https://abogadamagdiele.com",
+
     fonts: [
        {
             provider: fontProviders.google(),
