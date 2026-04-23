d616e886e2c40122d0b6c0251a778456e8e97d10
Keiner Mendoza <keinermendoza@gmail.com>
2026-04-22 20:53:26 -0400

    fix: add event listener in Navbar and FAQAcordion for astro swap event
    - remove unused js scripts for Navbar and FAQAcoridon components

--- src/assets/closeNavbar.js
+++
@@ -1,5 +0,0 @@
-const closeRadioBtn = document.getElementById('close-btn');
-document.querySelectorAll('.nav-anchor').forEach(anchor => {
-    anchor.onclick = () => closeRadioBtn.checked = true;
-});
-
